46b6d9e8bcea9cea0589ee54c20db411982ca85c
layla <layla@qq.com>
2023-01-08 14:44:55 +0800

    push commit

--- chui1/src/HelloWorld.java
+++ chui1/src/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
+        System.out.println("push2 test!");
     }
 }
